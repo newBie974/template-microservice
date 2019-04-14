426ce640ac2f4fc84a6e20659e413108e3549899
newBie974 <aymericelie.dijoux@gmail.com>
2019-04-14 13:49:14 +0200

    new config from class (#1)

--- index.ts
+++ index.ts
@@ -1,5 +1,13 @@
 'use strict';
 // Require app Launcher
 import appLauncher from './app/app';
+import myController from './app/controllers/myController';
 // Instantiate appLauncher
-const app = new appLauncher();
+const app = new appLauncher(
+    [ 
+        new myController() 
+    ], 
+    3030
+);
+
+app.listen();
