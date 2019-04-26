13fde0608cc89d9d8d7ffeb659ad6d89c8424a61
Dijoux <aymericelie.dijoux@gmail.com>
2019-04-26 23:17:37 +0200

    change function struct with async promises and add function get by id with error manage in request

--- app/controllers/myController.get.ts
+++ app/controllers/myController.get.ts
@@ -11,14 +11,25 @@ export default class myControllerGet {
 
     private initializeRoutes() {
         this.router.get('/', this.getAll);
+        this.router.get('/:id', this.getById);
     }
 
-    private getAll = async (request: express.Request, response: express.Response): Promise<express.Response> => {
+    private getAll = async(request: express.Request, response: express.Response): Promise<express.Response> => {
         try {
             const listController = await this.myControllerModel.find();
             return response.send(listController);
         } catch(err) {
             return response.send({ status: 404 });
+        }
+    }
+
+    private getById = async(request: express.Request, response: express.Response): Promise<express.Response> => {
+        const id = request.params.id;
+        try {
+            const controller = await this.myControllerModel.find({ _id: id});
+            return response.send(controller);
+        } catch(err) {
+            return response.status(404).send({ msg: 'no id found', code: 'NO_CONTROLLER_FOUND', err: err });
         }
     }
 }
